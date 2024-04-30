0cb53f8f8a95f71982de6981ace6d8607918841c
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-04-29 20:18:05 -0500

    Update Part-One.sh

--- Part-One.sh
+++ Part-One.sh
@@ -228,9 +228,9 @@ create_profile() {
     fi
 
     # Modify DEST if it contains /mnt to place profile in /mnt but use path without /mnt for KISS_PATH
-    KISS_PATH_DEST="$DEST/profile"
+    KISS_PATH_DEST="{$DEST}"
     if [[ "$DEST" == /mnt* ]]; then
-        PROFILE_FILE="${DEST}"
+        PROFILE_FILE="$DEST/profile"
         KISS_PATH_DEST="${DEST#/mnt}"  # Remove /mnt from the beginning of the DEST path
     else
         PROFILE_FILE="$DEST/profile"
